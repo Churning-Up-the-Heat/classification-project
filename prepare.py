0224345d95a390fb6843dbd56544f40e8a35da71
Yvonne K <yvonneking017@gmail.com>
2020-04-27 11:33:54 -0500

    created function to combine phone service and muliple lines

--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-def split_data(df, train_size, seed):
+def split_data(df, train_size=.8, seed=123):
     # Create the train and test sets
     train, test = train_test_split(df, train_size=train_size, random_state=seed)
     # Create the validate set by splitting from train set
@@ -24,7 +24,59 @@ def fix_dtypes(df):
     
     return df
 
-def prep_telco(df, train_size, seed):
+
+def create_phone_lines_variable(train, test, validate):
+    '''
+    takes in train, test, validate dataframes and creates a new column that represents
+    the information from phone_service and multiple_line in one variable 
+    returns train, test, and validate df
+    '''
+    #encode phone_service
+    train["phone_service"] = train.phone_service.str.replace('No', "0")
+    test["phone_service"] = test.phone_service.str.replace('No', "0")
+    validate["phone_service"] = validate.phone_service.str.replace('No', "0")
+    
+    train["phone_service"] = train.phone_service.str.replace('0 phone service', "0")
+    test["phone_service"] = test.phone_service.str.replace('0 phone service', "0")
+    validate["phone_service"] = validate.phone_service.str.replace('0 phone service', "0")
+    
+    train["phone_service"] = train.phone_service.str.replace('Yes', "1")
+    test["phone_service"] = test.phone_service.str.replace("Yes", "1")
+    validate["phone_service"] = validate.phone_service.str.replace("Yes", "1")
+    
+    #convert phone_service to an integer
+    train["phone_service"] = train.phone_service.astype("int")
+    test["phone_service"] = train.phone_service.astype("int")
+    validate["phone_service"] = train.phone_service.astype("int")
+    
+    #encode multiple_lines
+    train["multiple_lines"] = train.multiple_lines.str.replace('No', "0")
+    test["multiple_lines"] = test.multiple_lines.str.replace('No', "0")
+    validate["multiple_lines"] = validate.multiple_lines.str.replace('No', "0")
+    
+    train["multiple_lines"] = train.multiple_lines.str.replace('0 phone service', "0")
+    test["multiple_lines"] = test.multiple_lines.str.replace('0 phone service', "0")
+    validate["multiple_lines"] = validate.multiple_lines.str.replace('0 phone service', "0")
+    
+    train["multiple_lines"] = train.multiple_lines.str.replace('Yes', "1")
+    test["multiple_lines"] = test.multiple_lines.str.replace('Yes', "1")
+    validate["multiple_lines"] = validate.multiple_lines.str.replace('Yes', "1")
+    
+    #convert multiple_lines to an integer
+    train["multiple_lines"] = train.multiple_lines.astype("int")
+    test["multiple_lines"] = test.multiple_lines.astype("int")
+    validate["multiple_lines"] = validate.multiple_lines.astype("int")
+    
+    # create new variable by summing the encoded
+    train["phone_lines"] = train["phone_service"] + train["multiple_lines"]
+    validate["phone_lines"] = validate["phone_service"] + validate["multiple_lines"]
+    test["phone_lines"] = test["multiple_lines"] + test["phone_service"]
+    
+    return train, test, validate
+
+
+
+def prep_telco(df, train_size=.8, seed=123):
     
     #1. Split the data
     train, test, validate = split_data(df, train_size, seed)
@@ -40,9 +92,12 @@ def prep_telco(df, train_size, seed):
     validate = fix_dtypes(validate)
     
     #4. Add tenure years to each
-    train["tenure_years"] = (train.tenure / 12).round()
-    test["tenure_years"] = (test.tenure / 12).round()
-    validate["tenure_years"] = (validate.tenure / 12).round()
+    train["tenure_years"] = (train.tenure / 12).round(1)
+    test["tenure_years"] = (test.tenure / 12).round(1)
+    validate["tenure_years"] = (validate.tenure / 12).round(1)
+    
+    #5 Add phone_lines features
+    train, test, validate = create_phone_lines_variable(train, test, validate)
 
     return train, test, validate
 
