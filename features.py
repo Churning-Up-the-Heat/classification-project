18f3fdbf5819df9528a21ebf24590cef0fc728e2
shayaltshue <shayaltshue@gmail.com>
2020-04-28 15:29:14 -0500

    Removed online features, added automatic_payment

--- features.py
+++ features.py
@@ -1,6 +1,9 @@
-def online_features(train, test, validate):
-    train['online_features']    = train.online_security_encoded + train.device_protection_encoded + train.tech_support_encoded
-    validate['online_features'] = validate.online_security_encoded + validate.device_protection_encoded + validate.tech_support_encoded
-    test['online_features']     = test.online_security_encoded + test.device_protection_encoded + test.tech_support_encoded
-    
+def auto_payment(train, test, validate):
+    for df in [train, test, validate]:
+        df['automatic_payment'] = df.payment_type.str.contains('automatic').astype(int)
+    return train, test, validate
+
+def has_internet(train, test, validate):
+    for df in [train, test, validate]:
+            df['has_internet'] = ~ df.streaming_tv.str.contains('internet').astype(int)
     return train, test, validate
